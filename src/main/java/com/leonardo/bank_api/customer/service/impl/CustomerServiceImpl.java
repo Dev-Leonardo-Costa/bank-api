1ffd3d466a4e7cc50816908453573b365fcfa89c
Dev-Leonardo-Costa <devleonardo27@gmail.com>
2026-08-07 10:59:45 -0300

    chore: implentando os diagramas para documentar a arquitetura

--- src/main/java/com/leonardo/bank_api/customer/service/impl/CustomerServiceImpl.java
+++ src/main/java/com/leonardo/bank_api/customer/service/impl/CustomerServiceImpl.java
@@ -29,39 +29,14 @@ public class CustomerServiceImpl implements CustomerService {
 
         validateEmailEndCpf(request);
 
-//        Customer customer = Customer.builder()
-//                .fullName(request.fullName())
-//                .cpf(request.cpf())
-//                .email(request.email())
-//                .password(passwordEncoder.encode(request.password()))
-//                .build();
-//        customerRepository.save(customer);
-
         Customer customer = customerMapper.toEntity(request);
         customer.setPassword(passwordEncoder.encode(request.password()));
         customerRepository.save(customer);
         return customerMapper.toResponse(customer);
-
-//        return new CustomerResponse(
-//                customer.getId(),
-//                customer.getFullName(),
-//                customer.getCpf(),
-//                customer.getEmail(),
-//                customer.getCreatedAt().toLocalDate()
-//        );
     }
 
     @Override
     public CustomerResponse getCustomerById(Long id) {
-//        return customerRepository.findById(id)
-//                .map(customer -> new CustomerResponse(
-//                        customer.getId(),
-//                        customer.getFullName(),
-//                        customer.getCpf(),
-//                        customer.getEmail(),
-//                        customer.getCreatedAt().toLocalDate()
-//                ))
-//                .orElseThrow(() -> new RuntimeException("Cliente não encontrado"));
         Customer customer = customerRepository.findById(id)
                 .orElseThrow(() -> new ResourceNotFoundException("Cliente não encontrado"));
         return customerMapper.toResponse(customer);
