7f4981ee9e32a2ab879a0f98cb40f1a52310832e
Dev-Leonardo-Costa <devleonardo27@gmail.com>
2026-08-14 18:04:02 -0300

    feat: implementação de transação entre contas

---
+++ src/main/java/com/leonardo/bank_api/transaction/dto/request/DepositRequest.java
@@ -0,0 +1,15 @@
+package com.leonardo.bank_api.transaction.dto;
+
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+
+import java.math.BigDecimal;
+
+public record DepositRequest(
+
+        @NotNull(message = "Valor é obrigatório")
+        @Positive(message = "Valor deve ser maior que zero")
+        BigDecimal amount
+
+) {
+}
