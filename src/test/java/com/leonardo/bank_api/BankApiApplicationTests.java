5495d1c023212b42ceb858a1e40dd76676706621
Dev-Leonardo-Costa <devleonardo27@gmail.com>
2026-08-24 15:33:00 -0300

    ci: configura pipeline de build e testes com GitHub Actions

--- src/test/java/com/leonardo/bank_api/BankApiApplicationTests.java
+++ src/test/java/com/leonardo/bank_api/BankApiApplicationTests.java
@@ -2,11 +2,21 @@ package com.leonardo.bank_api;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.test.context.ActiveProfiles;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
-@ActiveProfiles("test")
 @SpringBootTest
+@Testcontainers
+@ActiveProfiles("test")
 class BankApiApplicationTests {
+
+	@Container
+	@ServiceConnection
+	static PostgreSQLContainer<?> postgres =
+			new PostgreSQLContainer<>("postgres:17");
 
 	@Test
 	void contextLoads() {
